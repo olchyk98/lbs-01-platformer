a101265246be1381f3a2402bac38f847734bfc37
Oles Odynets <oles@Oles-Odynets.lan>
2020-08-30 23:25:11 +0200

    PlayerInputEvents

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,17 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace Player
 {
+    /// <summary>
+    /// PlayerInput class is responsible for handling player keyboard events and informing child-components
+    /// about fk-actions.
+    /// </summary>
+    /// <future>
+    /// * Can be re-implemented using Unity Input System.
+    /// * Can be re-implemented using .GetButtonDown Event Structure.
+    /// </future>
     public class PlayerInput : MonoBehaviour
     {
-        private void Start()
-        {
-        
-        }
+        #region Fields
+        public UnityAction<float> OnHorizontalMove;
+        public UnityAction OnJump;
+        #endregion
 
+        #region RP Methods
         private void Update()
         {
-        
+            // Get horizontal movement
+            float horizontalValue = Input.GetAxis("Horizontal");
+            
+            // Inform all subscribers if movement input received
+            // Using Abs method to reduce float loss during zero comparison
+            if(Math.Abs(horizontalValue) > 0) OnHorizontalMove?.Invoke(horizontalValue);
+
+            // Check if space button is pressed
+            if (Input.GetKeyDown(KeyCode.Space)) OnJump?.Invoke();
         }
+        #endregion
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,18 +2,60 @@
 
 namespace Player
 {
+    [RequireComponent(typeof(PlayerInput))] // Used for keyboard inputs
+    [RequireComponent(typeof(Rigidbody))] // Used for physics
     public class PlayerMovement : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        #region Fields
         
-        }
+        #region Inspector Exposed Fields
+        [SerializeField] [Tooltip("Jump Height")] [Range(.1f, 1f)]
+        private float myJumpHeight;
 
-        // Update is called once per frame
-        void Update()
-        {
+        [SerializeField] [Tooltip("Movement Speed")] [Range(.1f, 1f)]
+        private float myMovementSpeed;
+        #endregion
         
+        #region Private Fields
+        private Rigidbody2D myRigidbody;
+        #endregion
+        
+        #endregion
+        
+        #region RP Methods
+        private void Start()
+        {
+            // :Subscribe to the input events:
+            // Get the component and temp store it
+            PlayerInput inputEvents = GetComponent<PlayerInput>();  
+            
+            // Subscribe to the horizontal movement event
+            inputEvents.OnHorizontalMove += MoveHorizontal;
+            
+            // Subscribe to the jump event
+            inputEvents.OnJump += Jump;
+            
+            // :Define Cache Fields:
+            // Rigidbody
+            myRigidbody = GetComponent<Rigidbody2D>();
+
         }
+        #endregion
+
+        #region Methods
+        public void MoveHorizontal(float aHorizontalValue)
+        {
+            // myRigidbody.MovePosition(GetComponent<Transform>().position + Vector3.right * (aHorizontalValue * myMovementSpeed));
+            myRigidbody.AddForce(Vector2.right * (aHorizontalValue * myMovementSpeed));
+        }
+        
+        /// <summary>
+        /// Makes player jump using its Rigidbody component.
+        /// </summary>
+        public void Jump()
+        {
+            myRigidbody.AddForce(Vector2.up * myJumpHeight, ForceMode2D.Impulse);
+        }
+        #endregion
     }
 }
